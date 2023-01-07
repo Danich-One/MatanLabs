b41554f482130d8aa5da39a43251e41eb543a38f
Danich-One <104724549+Danich-One@users.noreply.github.com>
2023-01-07 04:03:07 +0300

    Add files via upload

---
+++ LabaMatan3.0.py
@@ -0,0 +1,40 @@
+import numpy as np
+from shapely.geometry import Point, Polygon
+
+
+def f(x, y):
+    return x**2 + x*y + y**2
+
+def inside(x, y, points):
+    point = Point(x, y)
+    polygon = Polygon(points)
+    return polygon.contains(point)
+
+print('введите количество вершин многоугольника в области [−18, 17]×[−64, 71]:') # noqa
+num = input()
+num = int(num)
+
+bound = np.array([[-18, 17], [-64, 71]]) # noqa
+points = np.zeros((num, 2))
+I, h = 0, 1
+
+for i in range(num):
+    print('введите координаты', i+1 ,'точки (x, y) через пробел:') # noqa
+    points[i, 0], points[i, 1] = input().split()
+    while points[i, 0] < bound[0, 0] or points[i, 0] > bound[0, 1] or points[i, 1] < bound[1, 0] or points[i, 1] > bound[1, 1]:
+        print('вершина не в области G, введите заново:') # noqa
+        points[i, 0], points[i, 1] = map(float, input().split())
+
+X = np.arange(bound[0, 0]+h/2, bound[0, 1]+h/2, h)
+Y = np.arange(bound[1, 0]+h/2, bound[1, 1]+h/2, h)
+
+for x in X:
+    for y in Y:
+        if inside(x, y, points):
+            I += f(x, y) * 2 * h**2
+
+print('Интеграл = ', I)
+
+R = 85 * h**2 / 6
+
+print('Погрешность = ', R)
